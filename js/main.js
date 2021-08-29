7f9c69c0a65af6ef1ad92ed7305f03bda5849477
guardian-prodigy <devilmusicmutant@gmail.com>
2021-08-28 23:11:27 -0300

    added js

--- js/main.js
+++ js/main.js
@@ -171,3 +171,34 @@ function Sidebar() {
   });
 }
 
+// select links
+const scrollLinks = document.querySelectorAll(".filter-btn");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("class").slice(1);
+    const element = document.getElementById(id);
+
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    // close
+    
+  });
+});
+// calculate heights
